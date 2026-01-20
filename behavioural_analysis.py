cb90282e89595af0bff820b82fa0b7e13b3f81f3
joctavio287 <joctavio287@gmail.com>
2026-01-20 17:12:28 -0300

    check de consistencia

--- behavioural_analysis.py
+++ behavioural_analysis.py
@@ -2,12 +2,15 @@
 This script processes behavioural data from a PsychoPy experiment.
 It extracts metadata, performance and time trial information.
 """
+import matplotlib.pyplot as plt 
 from pathlib import Path
+import seaborn as sns
 import pandas as pd
+import numpy as np
 import ast
 
 from utils.processing  import (
-    dump_dict_to_json
+    dump_dict_to_json, load_json_to_dict
 )
 from utils.audio_helpers import read_ogg
 import config
@@ -51,7 +54,6 @@ for subject_file in config.PSYCHOPY_DIR.glob("*.csv"):
     subject_data = subject_data.iloc[1:-1]
 
     # Separate questionary and listening trials (rows)
-    answers_column = 'questions_trial.key_resp_questionary.keys'
     for j, row in subject_data.iterrows():
         exp = 'with_probe' if experiment=='DichoticListeningProbe' else 'no_probe'
         
@@ -102,11 +104,19 @@ for subject_file in config.PSYCHOPY_DIR.glob("*.csv"):
         )
         expected_duration = data.shape[0] / sample_rate
         listening_presentation['expected_audiobook_duration'].append(expected_duration)
-    print("Processed behavioural data for participant:", participant_id)
-    print("Expected durations vs actual durations (s):")
-    print(listening_presentation['expected_audiobook_duration'])
-    print(listening_presentation['audiobook_duration'])
+    
+    # Test whether the listening['expected_audiobook_duration'] differ from actual durations in an even number of samples
+    frame_rate = np.mean(subject_metadata['framerate'])
+    duration_diff = (
+        np.array(listening_presentation['expected_audiobook_duration']) - np.array(listening_presentation['audiobook_duration'])
+    )
+    duration_diff_samples = np.abs(np.round(duration_diff * frame_rate).astype(int))
+
+    print("\tProcessed behavioural data for participant:", participant_id)
+    print("\n\tDifference between actual and expected durations of audiobooks (in frames):\n")
+    print("\t\t", duration_diff_samples)
     print("\n")
+    
     # Save extracted data to .json files
     dump_dict_to_json(
         filepath=config.BEHAVIOURAL_DIR / f"{participant_id}_behavioural.json",
@@ -116,4 +126,96 @@ for subject_file in config.PSYCHOPY_DIR.glob("*.csv"):
             'listening_presentation': listening_presentation
         }
     )
-    
+
+# ========================================================================
+# Aggregate behavioural data across participants for analysis and plotting
+
+# Compile all participants scores into a single DataFrame
+score_rows = []
+for subj_json in config.BEHAVIOURAL_DIR.glob("*_behavioural.json"):
+    data = load_json_to_dict(subj_json)
+    pid = data["metadata"]["participant_id"]
+    scores = data["behaviour"]["questionary_evaluations"]
+    score_rows.extend(
+        {"correct": bool(v), "question_index": i % 3, "participant": pid}
+        for i, v in enumerate(scores)
+    )
+scores_data = pd.DataFrame(score_rows)
+scores_data.to_csv(
+    config.BEHAVIOURAL_DIR / "all_participants_behavioural_data.csv",
+    index=False
+)
+
+# Disaggregate scores by participant and question index
+scores_by_participant = (
+    scores_data
+        .rename(columns={"correct": r"Score Accuracy [$\%$]"})
+        .assign(**{r"Score Accuracy [$\%$]": lambda df: df[r"Score Accuracy [$\%$]"] * 1e2})
+        .groupby("participant", as_index=False)[r"Score Accuracy [$\%$]"]
+        .mean()
+)
+summary = (
+    scores_data
+        .groupby("question_index", as_index=False)["correct"]
+        .agg(mean="mean", sem=lambda s: s.std(ddof=1) / np.sqrt(s.count()))
+        # .agg(mean="mean", sem="sem")
+)
+
+# Plot behavioural performance across participants
+fig_path = config.FIGURES_DIR / "behavioural" / "performance_across_participants.png"
+fig_path.parent.mkdir(parents=True, exist_ok=True)
+
+behavioural_fig, axes = plt.subplots(
+    nrows=2, ncols=1,
+    figsize=(8, 6),
+    tight_layout=True
+)
+behavioural_fig.suptitle("Behavioural performance across participants", fontsize=12)
+histogram_plot = sns.histplot(
+    scores_by_participant[r"Score Accuracy [$\%$]"], 
+    bins=10, 
+    kde=False, 
+    # stat="count",
+    color='green',
+    alpha=0.4,
+    ax=axes[0]
+)
+_ = histogram_plot.axes.set_ylabel("Number of participants")
+_ = histogram_plot.axes.grid(True)
+
+stripplot = sns.stripplot(
+    data=scores_data, 
+    x="question_index", 
+    y="correct", 
+    color="black", 
+    alpha=0.3, 
+    jitter=0.2,
+    ax=axes[1]
+)
+errorbars = axes[1].errorbar(
+    summary["question_index"],
+    summary["mean"],
+    yerr=summary["sem"],
+    fmt="o",
+    color="black",
+    capsize=4,
+    linewidth=1,
+    zorder=5
+)
+_ = axes[1].legend(
+    [errorbars], 
+    ["Mean ± SEM"], 
+    loc=(.58, 0.45)
+)
+_ = axes[1].set_xlabel("Question Index")
+_ = axes[1].set_ylabel("Score(0=incorrect, 1=correct)")
+_ = axes[1].grid(
+    axis='y',
+    which='major',
+)
+
+behavioural_fig.savefig(
+    fig_path,
+    dpi=300
+)
+print(f"\tSaved behavioural figure in {fig_path.parent}")
